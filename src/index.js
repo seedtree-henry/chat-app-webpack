efae4d77ef18b0f67d57066dad377710ddbcccf7
henry <seedtree.henry@gmail.com>
2018-09-26 21:13:05 +1000

    client side - html & get message from the input

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 var openSocket = require("socket.io-client");
 var socket = openSocket("http://localhost:5220");
+
+var messagesUlTag = document.querySelector(".messagesList");
+var messageFormTag = document.querySelector(".newMessageForm");
+var messageInputTag = messageFormTag.querySelector("input");
+
+messageFormTag.addEventListener("submit", submitMessage);
+
+function submitMessage(event) {
+    event.preventDefault();
+    var message = messageInputTag.value;
+    console.log(message);
+}
